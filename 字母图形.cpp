bebc7dd09b8b78aa5de15106f999c49b9b9ba2d0
xwolalala <3206574146@qq.com>
2020-01-14 11:19:43 +0800

    字母图形

---
+++ 字母图形.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(abs(i-j)==0)
+				cout<<'A';
+			if(abs(i-j)==1)
+				cout<<'B';
+			if(abs(i-j)==2)
+				cout<<'C';
+			if(abs(i-j)==3)
+				cout<<'D';
+			if(abs(i-j)==4)
+				cout<<'E';
+			if(abs(i-j)==5)
+				cout<<'F';
+			if(abs(i-j)==6)
+				cout<<'G';
+			if(abs(i-j)==7)
+				cout<<'H';
+			if(abs(i-j)==8)
+				cout<<'I';
+			if(abs(i-j)==9)
+				cout<<'J';
+			if(abs(i-j)==10)
+				cout<<'K';
+			if(abs(i-j)==11)
+				cout<<'L';
+			if(abs(i-j)==12)
+				cout<<'M';
+			if(abs(i-j)==13)
+				cout<<'N';
+			if(abs(i-j)==14)
+				cout<<'O';
+			if(abs(i-j)==15)
+				cout<<'P';
+			if(abs(i-j)==16)
+				cout<<'Q';
+			if(abs(i-j)==17)
+				cout<<'R';
+			if(abs(i-j)==18)
+				cout<<'S';
+			if(abs(i-j)==19)
+				cout<<'T';
+			if(abs(i-j)==20)
+				cout<<'U';
+			if(abs(i-j)==21)
+				cout<<'V';
+			if(abs(i-j)==22)
+				cout<<'W';
+			if(abs(i-j)==23)
+				cout<<'X';
+			if(abs(i-j)==24)
+				cout<<'Y';
+			if(abs(i-j)==25)
+				cout<<'Z';
+		}
+		cout<<endl;
+	}
+	return 0;
+}
