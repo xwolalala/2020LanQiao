22c976f34f4d39fae2712a5b3df5e00758cc234b
xwolalala <3206574146@qq.com>
2020-01-16 20:23:37 +0800

    特殊的数字

---
+++ 特殊的数字.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	for(int i=100;i<=999;i++)
+	{
+		a=i/100;
+		b=(i-a*100)/10;
+		c=i%10;
+		if(i==a*a*a+b*b*b+c*c*c)
+			cout<<i<<endl;
+	}
+	return 0;
+}
