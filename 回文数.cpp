a499ba3e6ad392bd7f7bb635a8fbffa674d549a2
xwolalala <3206574146@qq.com>
2020-01-16 20:24:41 +0800

    回文数

---
+++ 回文数.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	for(int i=1000;i<=9999;i++)
+	{
+		a=i/1000;
+		b=(i%1000)/100;
+		c=(i%100)/10;
+		d=i%10;
+		if(a==d&&b==c)
+			cout<<i<<endl;
+	}
+	return 0;
+}
